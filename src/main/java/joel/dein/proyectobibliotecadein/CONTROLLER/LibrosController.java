92f8e16b36d44801ec6bec6d2218de300ad4053b
Joelgs13 <050213jgonzalez@gmail.com>
2025-01-20 19:37:05 +0100

    Interfaz libros ok

---
+++ src/main/java/joel/dein/proyectobibliotecadein/CONTROLLER/LibrosController.java
@@ -0,0 +1,50 @@
+package joel.dein.proyectobibliotecadein.CONTROLLER;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
+public class LibrosController {
+
+    @FXML
+    private CheckBox cbBajaLibro;
+
+    @FXML
+    private ComboBox<?> cbEstadoLibro;
+
+    @FXML
+    private ImageView ivImagenDePortada;
+
+    @FXML
+    private TextField tfAutorLibro;
+
+    @FXML
+    private TextField tfEditorialLibro;
+
+    @FXML
+    private TextField tfTituloLibro;
+
+    @FXML
+    void aniadirImagen(ActionEvent event) {
+
+    }
+
+    @FXML
+    void borrarImagen(ActionEvent event) {
+
+    }
+
+    @FXML
+    void cancelar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void guardarCambios(ActionEvent event) {
+
+    }
+
+}
