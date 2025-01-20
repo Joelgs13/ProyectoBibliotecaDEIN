5b653c514528b92e5fb530376de95969d698b558
Joelgs13 <050213jgonzalez@gmail.com>
2025-01-20 19:00:05 +0100

    Pantalla principal hecha

---
+++ src/main/java/joel/dein/proyectobibliotecadein/CONTROLLER/BibliotecaController.java
@@ -0,0 +1,122 @@
+package joel.dein.proyectobibliotecadein.CONTROLLER;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+public class BibliotecaController {
+
+    @FXML
+    private ComboBox<?> cbFiltroHistorico;
+
+    @FXML
+    private TableView<?> tablaAlumnos;
+
+    @FXML
+    private TableView<?> tablaDevoluciones;
+
+    @FXML
+    private TableView<?> tablaHistorico;
+
+    @FXML
+    private TableView<?> tablaLibros;
+
+    @FXML
+    private TableView<?> tablaPrestamos;
+
+    @FXML
+    private TextField tfFiltrarAlumno;
+
+    @FXML
+    private TextField tfFiltrarDevolucion;
+
+    @FXML
+    private TextField tfFiltrarLibros;
+
+    @FXML
+    private TextField tfFiltrarPrestamo;
+
+    @FXML
+    void aniadirAlumno(ActionEvent event) {
+
+    }
+
+    @FXML
+    void aniadirLibro(ActionEvent event) {
+
+    }
+
+    @FXML
+    void bajaLibro(ActionEvent event) {
+
+    }
+
+    @FXML
+    void borrarLibro(ActionEvent event) {
+
+    }
+
+    @FXML
+    void devolverLibro(ActionEvent event) {
+
+    }
+
+    @FXML
+    void filtrarAlumno(ActionEvent event) {
+
+    }
+
+    @FXML
+    void filtrarDevolucion(ActionEvent event) {
+
+    }
+
+    @FXML
+    void filtrarHistorico(ActionEvent event) {
+
+    }
+
+    @FXML
+    void filtrarLibros(ActionEvent event) {
+
+    }
+
+    @FXML
+    void filtrarPrestamo(ActionEvent event) {
+
+    }
+
+    @FXML
+    void modificarAlumno(ActionEvent event) {
+
+    }
+
+    @FXML
+    void modificarLibro(ActionEvent event) {
+
+    }
+
+    @FXML
+    void populateFieldsAlumno(MouseEvent event) {
+
+    }
+
+    @FXML
+    void populateFieldsLibro(MouseEvent event) {
+
+    }
+
+    @FXML
+    void populateFieldsPrestamo(MouseEvent event) {
+
+    }
+
+    @FXML
+    void prestarLibro(ActionEvent event) {
+
+    }
+
+}
