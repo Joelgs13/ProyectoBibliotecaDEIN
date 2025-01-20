ddc3d814865f81a4b72e3f6d5371742b587d3d44
Joelgs13 <050213jgonzalez@gmail.com>
2025-01-20 20:26:16 +0100

    Interfaces de prestamo y alumnos ok
    (pendiente ver interfaaz de devolucion y la del historico y como gestionarlo)

---
+++ src/main/java/joel/dein/proyectobibliotecadein/CONTROLLER/AlumnosController.java
@@ -0,0 +1,31 @@
+package joel.dein.proyectobibliotecadein.CONTROLLER;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class AlumnosController {
+
+    @FXML
+    private TextField tfDniAlumno;
+
+    @FXML
+    private TextField tfNombreAlumno;
+
+    @FXML
+    private TextField tfPrimerApellidoAlumno;
+
+    @FXML
+    private TextField tfSegundoApellidoAlumno;
+
+    @FXML
+    void cancelar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void guardarAlumno(ActionEvent event) {
+
+    }
+
+}
---
+++ src/main/java/joel/dein/proyectobibliotecadein/CONTROLLER/PrestamoController.java
@@ -0,0 +1,29 @@
+package joel.dein.proyectobibliotecadein.CONTROLLER;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+
+public class PrestamoController {
+
+    @FXML
+    private ComboBox<?> cbEstudiante;
+
+    @FXML
+    private ComboBox<?> cbLibro;
+
+    @FXML
+    private DatePicker doFechaPrestamo;
+
+    @FXML
+    void cancelar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void guardarCambios(ActionEvent event) {
+
+    }
+
+}
