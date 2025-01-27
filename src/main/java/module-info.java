a9a004f6f55d8a5f39ee744364f99b774eccc7f5
Joelgs13 <050213jgonzalez@gmail.com>
2025-01-27 17:02:13 +0100

    arreglado dependencias despues de realizar el segundo informe jasper
    (pendiente ver interfaaz de devolucion y la del historico y como gestionarlo)

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module joel.dein.proyectobibliotecadein {
     requires javafx.fxml;
     requires java.sql;
     requires net.sf.jasperreports.core;
+    requires jasperreports;
 
 
     opens joel.dein.proyectobibliotecadein to javafx.fxml;
