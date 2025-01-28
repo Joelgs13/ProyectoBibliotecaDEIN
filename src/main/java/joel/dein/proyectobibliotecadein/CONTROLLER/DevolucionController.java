c2d3b1c48db3d490ea107f690f4492f0c60d612a
Joelgs13 <050213jgonzalez@gmail.com>
2025-01-28 18:42:56 +0100

    interfaz devolucion añadida

---
+++ src/main/java/joel/dein/proyectobibliotecadein/CONTROLLER/DevolucionController.java
@@ -0,0 +1,26 @@
+package joel.dein.proyectobibliotecadein.CONTROLLER;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+
+public class DevolucionController {
+
+    @FXML
+    private ComboBox<?> cbEstudiante;
+
+    @FXML
+    private DatePicker doFechaPrestamo;
+
+    @FXML
+    void cancelar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void guardarCambios(ActionEvent event) {
+
+    }
+
+}
